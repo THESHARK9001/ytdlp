6f4ef6236206b237b449378103396910cf669549
THESHARK9001 <76677889+THESHARK9001@users.noreply.github.com>
2024-09-16 00:06:43 -0700

    Update yt.py
    deleted unnecessary lines and changed "b" to "ba+bv"

--- ytdlpbuild/yt.py
+++ ytdlpbuild/yt.py
@@ -2,24 +2,18 @@ import yt_dlp
 import os
 from os import path
 import shutil
-
 def youtube_downloader():
     fullpath = os.path.join
     python_directory = "./videos"
     start_directory = "./"
-
     URLS = input("Link: ")
-
     ydl_opts = {
-        'format': 'b',
+        'format': 'ba+bv',
     }
-
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         error_code = ydl.download(URLS)
-
     if not path.exists("./videos"):
         os.mkdir("./videos")
-
     def main():
         for dirname, dirnames, filenames in os.walk(start_directory):
             for filename in filenames:
@@ -27,5 +21,4 @@ def youtube_downloader():
                 if filename.endswith("mp4"):
                     shutil.move(source, fullpath(python_directory, filename))
     main()
-
 youtube_downloader()
