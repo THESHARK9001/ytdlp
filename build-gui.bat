e3320f93f5f026b5953625b371ec7cad58f47c6a
THESHARK9001 <76677889+THESHARK9001@users.noreply.github.com>
2024-10-26 23:51:07 -0700

    Added build-gui.bat for building gui

---
+++ build-gui.bat
@@ -0,0 +1,12 @@
+@echo Please make sure you have python installed before continuing
+@pause
+@echo Please make sure you do not have a VPN while using yt-gui.py 
+@pause
+@python.exe -m pip install --upgrade pip
+@pip install yt-dlp pyinstaller
+@pyinstaller --clean --onefile --noconsole .\ytdlpbuild\yt-gui.py
+@del yt-gui.spec
+@echo y|rmdir /s build
+@copy .\dist\yt.exe .\yt-gui.exe
+@echo y|rmdir /s dist
+@pause
