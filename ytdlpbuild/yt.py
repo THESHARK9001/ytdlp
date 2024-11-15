374660024ea218b47de4dbe8c8ca043e894c800a
THESHARK9001 <76677889+THESHARK9001@users.noreply.github.com>
2024-11-14 23:32:59 -0800

    Update yt.py
    removed putting video files into a folder called "videos"

--- ytdlpbuild/yt.py
+++ ytdlpbuild/yt.py
@@ -1,24 +1,9 @@
 import yt_dlp
-import os
-from os import path
-import shutil
 def youtube_downloader():
-    fullpath = os.path.join
-    python_directory = "./videos"
-    start_directory = "./"
     URLS = input("Link: ")
     ydl_opts = {
-        'format': 'ba+bv',
+        'format': 'bv+ba',
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         error_code = ydl.download(URLS)
-    if not path.exists("./videos"):
-        os.mkdir("./videos")
-    def main():
-        for dirname, dirnames, filenames in os.walk(start_directory):
-            for filename in filenames:
-                source = fullpath(dirname, filename)
-                if filename.endswith("mp4"):
-                    shutil.move(source, fullpath(python_directory, filename))
-    main()
 youtube_downloader()
