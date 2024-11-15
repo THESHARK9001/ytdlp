9c1ae2a3882ab72273fa711c8240d3273906c135
THESHARK9001 <76677889+THESHARK9001@users.noreply.github.com>
2024-11-14 23:35:26 -0800

    Update build-gui.bat

--- build-gui.bat
+++ build-gui.bat
@@ -4,7 +4,7 @@
 @pause
 @python.exe -m pip install --upgrade pip
 @pip install yt-dlp pyinstaller
-@pyinstaller --clean --onefile --noconsole --icon=.\ytdlpbuild\Icon.ico .\ytdlpbuild\yt-gui.pyw
+@pyinstaller --clean --onefile --noconsole --icon=.\ytdlpbuild\Icon.ico .\ytdlpbuild\yt-gui.py
 @del yt-gui.spec
 @echo y|rmdir /s build
 @copy .\dist\yt-gui.exe .\yt-gui.exe
